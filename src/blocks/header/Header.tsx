db018af787bf08f99b951c41ac2ef354e42300bc
Aloxan12 <kvakvaaak@mail.ru>
2024-02-08 23:24:32 +0300

    Poiret One

--- src/blocks/header/Header.tsx
+++ src/blocks/header/Header.tsx
@@ -5,7 +5,6 @@ import TextPhoto from '../../assets/images/text.png'
 export const Header = () => {
     return (
         <header className='page header'>
-            <div className='title'><span>26 июля 2024 года</span></div>
             <div className='content'>
                 <img src={TextPhoto} className='text' alt='' />
                 <img src={HeaderPhoto} className='photo'  alt=''/>
