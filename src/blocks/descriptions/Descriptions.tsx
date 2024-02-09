bee9585b441a65fe9f295ddec2fddf5329d34723
Aloxan12 <kvakvaaak@mail.ru>
2024-02-09 20:10:06 +0300

    Poiret One

--- src/blocks/descriptions/Descriptions.tsx
+++ src/blocks/descriptions/Descriptions.tsx
@@ -4,7 +4,7 @@ import photo from '../../assets/images/photo2.png'
 export const Descriptions = () => {
     return (
         <div className='page descriptions'>
-            <h3>Дорогой <br/> гость</h3>
+            <h3>Дорогой <br/> гость!</h3>
             <p>Мы рады сообщить Вам, что 26.07.2024 состоится самое главное торжество в нашей жизни - день нашей свадьбы! Приглашаем Вас разделить с нами радость этого незабываемого дня!</p>
             <img src={photo} alt='' className='photo'/>
         </div>
